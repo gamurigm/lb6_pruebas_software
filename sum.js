ff62e0768b77bad3c898136cc4c7e4780908da75
Gabriel Murillo Medina <gamurigm@gmail.com>
2026-01-22 08:29:48 -0500

    Add multiply function and tests

--- sum.js
+++ sum.js
@@ -1,3 +1,8 @@
 export function sum(a, b) {
     return a + b;
 }
+
+export function multiply(a, b) {
+    return a * b;
+}
+
--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,4 @@
-import { sum } from './sum.js';
+import { sum, multiply } from './sum.js';
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
@@ -7,3 +7,8 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds -1 + 5 to equal 4', () => {
     expect(sum(-1, 5)).toBe(4);
 });
+
+test('multiplies 2 * 3 to equal 6', () => {
+    expect(multiply(2, 3)).toBe(6);
+});
+
